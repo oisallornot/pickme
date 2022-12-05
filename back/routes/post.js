c0ee674e13b2b3e68dc493aa1698e53b50b8e5d2
oisallornot <a@a.com>
2022-12-05 18:24:42 +0900

    s3sdsssssssssssssssssssss\

--- back/routes/post.js
+++ back/routes/post.js
@@ -38,6 +38,7 @@ const upload = multer({
 router.post('/images',isLoggedIn,upload.array('image'),(req,res,next)=>{
     console.log(req.files);
     res.json(req.files.map((v)=>  v.location.replace(/\/original\//, '/thumb/') ))
+    
 
 })
 
