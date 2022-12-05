e6e89fa42881923c3421a19518aa9f52be995896
oisallornot <a@a.com>
2022-12-05 12:36:50 +0900

    s3sdssssssssssss

--- back/routes/post.js
+++ back/routes/post.js
@@ -6,7 +6,7 @@ const{isLoggedIn} =require('./middlewares')
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-// const multerS3 = require('multer-s3');
+const multerS3 = require('multer-s3');
 const AWS= require('aws-sdk');
 
 
