fdf25ae9953c361b1445075146f7da5afa8aef1a
oisallornot <a@a.com>
2022-12-05 12:18:25 +0900

    s3sdssss

--- back/routes/post.js
+++ back/routes/post.js
@@ -6,8 +6,8 @@ const{isLoggedIn} =require('./middlewares')
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-// const multerS3 = require('multer-s3');
-// const AWS= require('aws-sdk');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
 
 
 try{
@@ -17,19 +17,20 @@ try{
     console.log('upload 폴더가 없습니다.')
     fs.mkdirSync('uploads')
 }
+AWS.config.update({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    region: 'ap-northeast-2',
+  });
 
 const upload = multer({
-    storage:multer.diskStorage({
-        destination(req,file,done){
-            done(null,'uploads');
-        },
-        filename(req,file,done){
-            const ext =path.extname(file.originalname); //확장자추출(png)
-            const basename =path.basename(file.originalname, ext);
-
-            done(null,basename+ '_'+ new Date().getTime() +ext);
-        }
-    }),
+storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: 'react-pickme-s3',
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`)
+    }
+  }),
     limits: {fileSize: 20 * 1024 * 1024},
 })
 
@@ -188,7 +189,7 @@ router.post('/:postId/comment',isLoggedIn,async(req,res,next)=>{
 
 router.post('/images',isLoggedIn,upload.array('image'),(req,res,next)=>{
     console.log(req.files);
-    res.json(req.files.map((v)=>v.filename ))
+    res.json(req.files.map((v)=>v.location ))
 })
 
 
