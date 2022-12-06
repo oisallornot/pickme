4481cb24a0cac65c12a2a75abde68e5e8149724c
oisallornot <a@a.com>
2022-12-06 12:48:43 +0900

    dfdssss

--- back/app.js
+++ back/app.js
@@ -1,86 +1,73 @@
 const express = require('express');
 const cors = require('cors');
-const port = 3065;
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const path = require('path');
+const hpp = require('hpp');
+const helmet = require('helmet');
+
 const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
 const userRouter = require('./routes/user');
 const hashtagRouter = require('./routes/hashtag');
-const passport = require('passport');
 const db = require('./models');
 const passportConfig = require('./passport');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const app =express();
-const path=require('path');
-const hpp = require('hpp');
-const helmet = require('helmet');
-dotenv.config();
 
+dotenv.config();
+const app = express();
 db.sequelize.sync()
-.then(()=>{
-    console.log('db연결성공!!!!');
-})
-.catch(console.error);
+  .then(() => {
+    console.log('db 연결 성공');
+  })
+  .catch(console.error);
 passportConfig();
 
-
-if(process.env.NODE_ENV==='production'){
-    app.enable('trust proxy');
-    app.use(morgan('combined'));
-    app.use(hpp());
-    app.use(helmet({ contentSecurityPolicy: false }));
-    
-    app.use(cors({
-        origin: 'https://bitfrommind.com',
-        credentials:true,
-    }));
-}else{
-    app.use(morgan('dev'));
-    app.use(cors({
-        origin: true,
-        credentials:true,
-    }));
-    
+if (process.env.NODE_ENV === 'production') {
+  app.enable('trust proxy');
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(cors({
+    origin: 'https://bitfrommind.com',
+    credentials: true,
+  }));
+} else {
+  app.use(morgan('dev'));
+  app.use(cors({
+    origin: true,
+    credentials: true,
+  }));
 }
-
-
-app.use('/',express.static(path.join(__dirname,'uploads')))
-
+app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    saveUninitialized:false,
-    resave:false,
-    secret:process.env.COOKIE_SECRET,
-    proxy: process.env.NODE_ENV === 'production',
-    cookie:{
-        httpOnly:true,
-        secure:false,
-        domain: process.env.NODE_ENV==='production' &&'.bitfrommind.com'
-    }
+  saveUninitialized: false,
+  resave: false,
+  secret: process.env.COOKIE_SECRET,
+  proxy: process.env.NODE_ENV === 'production',
+  cookie: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    domain: process.env.NODE_ENV === 'production' && '.bitfrommind.com'
+  },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.get('/',(req,res)=>{
-    res.send('heelo');
+app.get('/', (req, res) => {
+  res.send('hello express');
 });
 
+app.use('/posts', postsRouter);
+app.use('/post', postRouter);
+app.use('/user', userRouter);
+app.use('/hashtag', hashtagRouter);
 
-
-
-app.use('/posts',postsRouter);
-app.use('/post',postRouter);
-app.use('/user',userRouter);
-app.use('/hashtag',hashtagRouter);
-
-
-
-
-app.listen(port,()=>{
-    console.log(`server is running with port No.${port}`)
+app.listen(3065, () => {
+  console.log('서버 실행 중!');
 });
